f0f82b8f1428c840affe0636c05c235862b3862d
StekAlaZarii <101180506+StekAlaZarii@users.noreply.github.com>
2022-06-14 09:29:14 +0000

    Commit5_3

--- script.js
+++ script.js
@@ -15,25 +15,35 @@
   })
 
   cw1.addEventListener("click", function () {
-    answer.innerHTML="Loading...";
+    answer.innerHTML="Ładowanie...";
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response=>response.json())
-        .then(function (data){
-          NowaLinia(data)
-          console.log(data)
+        .then(function (dane){
+          NowaLinia(dane)
         })
-    function NowaLinia(data){
+    function NowaLinia(dane){
       answer.innerHTML="";
-      for (var i =0; i <data.length;i++){
+      for (var i =0; i <dane.length;i++){
         var div = document.createElement("div");
-        div.innerHTML= 'UserID: ' + data[i].userId + '<br>id: ' + data[i].id +'</br>' + ' title: ' + data[i].title + '<br> body: '+ data[i].body + '<br><br>';
+        div.innerHTML= 'UserID: ' + dane[i].userId + '<br>id: ' + dane[i].id +'</br>' + ' title: ' + dane[i].title + '<br> body: '+ dane[i].body + '<br><br>';
         answer.appendChild(div);
       }
     }
   })
 
   cw2.addEventListener("click", function () {
-    //TODO implement it
+    answer.innerHTML="Ładowanie...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response=>response.json())
+        .then(function (dane){
+          NowaLinia(dane)
+        })
+    function NowaLinia(dane){
+      answer.innerHTML="";
+      var div = document.createElement("div"); div.innerHTML = 'UserID: ' + dane[0].userId + 'id: ' + dane[0].id + ' <br> title: ' + dane[0].title + '<br> body: '+ dane[0].body + '<br><br>';
+      answer.appendChild(div);
+      
+    }
   })
 
   cw3.addEventListener("click", function () {
